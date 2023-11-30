81cf3ae530d64f8cb9041c03c22f59a76cffed64
Zmaxtm <150814203+Zmaxtm@users.noreply.github.com>
2023-11-30 20:17:19 +0800

    Update .umirc.ts

--- .umirc.ts
+++ .umirc.ts
@@ -17,37 +17,13 @@ export default defineConfig({
  
     'prop-types': 'var window.PropTypes',
  
-    '@alifd/next': 'var window.Next',
- 
-    '@alilc/lowcode-engine': 'var window.AliLowCodeEngine',
- 
-    '@alilc/lowcode-editor-core': 'var window.AliLowCodeEngine.common.editorCabin',
- 
-    '@alilc/lowcode-editor-skeleton': 'var window.AliLowCodeEngine.common.skeletonCabin',
- 
-    '@alilc/lowcode-designer': 'var window.AliLowCodeEngine.common.designerCabin',
- 
-    '@alilc/lowcode-engine-ext': 'var window.AliLowCodeEngineExt',
- 
-    '@ali/lowcode-engine': 'var window.AliLowCodeEngine',
- 
     'moment': 'var window.moment',
  
     'lodash': 'var window._',
  
   },
  
-  styles: [
- 
-    'https://alifd.alicdn.com/npm/@alilc/lowcode-engine@latest/dist/css/engine-core.css',
- 
-    'https://g.alicdn.com/code/lib/alifd__next/1.23.24/next.min.css',
- 
-    'https://alifd.alicdn.com/npm/@alifd/theme-lowcode-light/0.2.0/next.min.css',
- 
-    'https://alifd.alicdn.com/npm/@alilc/lowcode-engine-ext@latest/dist/css/engine-ext.css'
- 
-  ],
+
  
   scripts: [
  
@@ -98,30 +74,7 @@ export default defineConfig({
       defer: false,
  
     },
- 
-    {
- 
-      src: 'https://g.alicdn.com/code/lib/alifd__next/1.23.24/next.min.js',
- 
-      defer: false,
- 
-    },
- 
-    {
- 
-      src: 'https://alifd.alicdn.com/npm/@alilc/lowcode-engine@latest/dist/js/engine-core.js',
- 
-      defer: false,
- 
-    },
- 
-    {
- 
-      src: 'https://alifd.alicdn.com/npm/@alilc/lowcode-engine-ext@latest/dist/js/engine-ext.js',
- 
-      defer: false,
- 
-    },],
+ ],
   routes: [
     {
       path: '/',
